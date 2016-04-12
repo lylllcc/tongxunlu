71be00390c047653f269530f4d9c48e9b3ab06ff
yyljj <yyljj@outlook.com>
2016-04-12 21:45:07 +0800

    大体框架

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>通讯录</title>
+</head>
+<body>
+    <h2>中国石油大学(华东)通讯录</h2>
+    <form action="con.php" method="post">
+        <input type="text" placeholder="请输入姓名或部门" name="department"><br>
+        <input type="submit" value="查询">
+    </form>
+
+</body>
+</html>
